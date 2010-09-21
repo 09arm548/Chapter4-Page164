f02f78187e1a0f78fd3f69f4cbee0dc345dc7b32
AndreaMcPherson <arm20@sage.southhills.edu>
2010-09-21 16:12:52 -0400

    clean up formatting

---
+++ ch4 backup.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRental
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDispRentalRates = New System.Windows.Forms.Button()
+        Me.btnDispCustBill = New System.Windows.Forms.Button()
+        Me.lstPrice = New System.Windows.Forms.ListBox()
+        Me.lstBill = New System.Windows.Forms.ListBox()
+        Me.txtItem = New System.Windows.Forms.TextBox()
+        Me.txtDuration = New System.Windows.Forms.TextBox()
+        Me.lblItem = New System.Windows.Forms.Label()
+        Me.lblDuration = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnDispRentalRates
+        '
+        Me.btnDispRentalRates.Location = New System.Drawing.Point(97, 18)
+        Me.btnDispRentalRates.Name = "btnDispRentalRates"
+        Me.btnDispRentalRates.Size = New System.Drawing.Size(175, 65)
+        Me.btnDispRentalRates.TabIndex = 0
+        Me.btnDispRentalRates.Text = "Display Rental Rates"
+        Me.btnDispRentalRates.UseVisualStyleBackColor = True
+        '
+        'btnDispCustBill
+        '
+        Me.btnDispCustBill.Location = New System.Drawing.Point(97, 256)
+        Me.btnDispCustBill.Name = "btnDispCustBill"
+        Me.btnDispCustBill.Size = New System.Drawing.Size(175, 61)
+        Me.btnDispCustBill.TabIndex = 1
+        Me.btnDispCustBill.Text = "Display Customer Bill"
+        Me.btnDispCustBill.UseVisualStyleBackColor = True
+        '
+        'lstPrice
+        '
+        Me.lstPrice.FormattingEnabled = True
+        Me.lstPrice.ItemHeight = 14
+        Me.lstPrice.Location = New System.Drawing.Point(479, 18)
+        Me.lstPrice.Name = "lstPrice"
+        Me.lstPrice.Size = New System.Drawing.Size(341, 102)
+        Me.lstPrice.TabIndex = 2
+        '
+        'lstBill
+        '
+        Me.lstBill.FormattingEnabled = True
+        Me.lstBill.ItemHeight = 14
+        Me.lstBill.Location = New System.Drawing.Point(479, 215)
+        Me.lstBill.Name = "lstBill"
+        Me.lstBill.Size = New System.Drawing.Size(341, 102)
+        Me.lstBill.TabIndex = 3
+        '
+        'txtItem
+        '
+        Me.txtItem.Location = New System.Drawing.Point(275, 107)
+        Me.txtItem.Name = "txtItem"
+        Me.txtItem.Size = New System.Drawing.Size(47, 20)
+        Me.txtItem.TabIndex = 4
+        '
+        'txtDuration
+        '
+        Me.txtDuration.Location = New System.Drawing.Point(275, 179)
+        Me.txtDuration.Name = "txtDuration"
+        Me.txtDuration.Size = New System.Drawing.Size(47, 20)
+        Me.txtDuration.TabIndex = 5
+        '
+        'lblItem
+        '
+        Me.lblItem.AutoSize = True
+        Me.lblItem.Location = New System.Drawing.Point(65, 107)
+        Me.lblItem.Name = "lblItem"
+        Me.lblItem.Size = New System.Drawing.Size(196, 14)
+        Me.lblItem.TabIndex = 6
+        Me.lblItem.Text = "Select an item (1, 2, or3):"
+        '
+        'lblDuration
+        '
+        Me.lblDuration.AutoSize = True
+        Me.lblDuration.Location = New System.Drawing.Point(62, 179)
+        Me.lblDuration.Name = "lblDuration"
+        Me.lblDuration.Size = New System.Drawing.Size(196, 14)
+        Me.lblDuration.TabIndex = 7
+        Me.lblDuration.Text = "Select a duration (H or F):"
+        '
+        'frmRental
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(922, 392)
+        Me.Controls.Add(Me.lblDuration)
+        Me.Controls.Add(Me.lblItem)
+        Me.Controls.Add(Me.txtDuration)
+        Me.Controls.Add(Me.txtItem)
+        Me.Controls.Add(Me.lstBill)
+        Me.Controls.Add(Me.lstPrice)
+        Me.Controls.Add(Me.btnDispCustBill)
+        Me.Controls.Add(Me.btnDispRentalRates)
+        Me.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "frmRental"
+        Me.Text = "Welcome to Eddie's Equipment Rental"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnDispRentalRates As System.Windows.Forms.Button
+    Friend WithEvents btnDispCustBill As System.Windows.Forms.Button
+    Friend WithEvents lstPrice As System.Windows.Forms.ListBox
+    Friend WithEvents lstBill As System.Windows.Forms.ListBox
+    Friend WithEvents txtItem As System.Windows.Forms.TextBox
+    Friend WithEvents txtDuration As System.Windows.Forms.TextBox
+    Friend WithEvents lblItem As System.Windows.Forms.Label
+    Friend WithEvents lblDuration As System.Windows.Forms.Label
+
+End Class
---
+++ ch4 backup.vb
@@ -0,0 +1,102 @@
+﻿Public Class frmRental
+
+    Private Sub btnDispRentalRates_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDispRentalRates.Click
+
+        Dim readervar As IO.StreamReader = IO.File.OpenText("../../Data.txt")
+        Dim strFormat As String
+        strFormat = "{0, -10} {1, 15} {2, 15}"
+
+        'Try
+
+        'Catch ex As Exception
+        '    MessageBox.Show("sdkljf")
+        'End Try
+
+        'build price table
+        lstPrice.Items.Add(String.Format(strFormat, "PRICE", "HALF", "FULL"))
+        lstPrice.Items.Add(String.Format(strFormat, "RUG CLEANER", (FormatCurrency("16")), FormatCurrency("24")))
+        lstPrice.Items.Add(String.Format(strFormat, "LAWN MOWER", (FormatCurrency("12")), FormatCurrency("18")))
+        lstPrice.Items.Add(String.Format(strFormat, "PAINT SPRAYER", (FormatCurrency("20")), FormatCurrency("30")))
+
+
+    End Sub
+
+    Private Sub btnDispCustBill_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDispCustBill.Click
+
+        Dim intItem As Integer = CInt(CStr(txtItem.Text))
+        Dim dblDeposit As Double = 30.0
+        Dim dblTotal As Double = 0
+
+
+        lstBill.Items.Add("RECEIPT")
+
+        If intItem <> 1 Or intItem <> 2 Or intItem <> 3 Then
+            MessageBox.Show("You must enter 1, 2, or 3")
+        End If
+        If txtDuration.Text <> "h" Or txtDuration.Text <> "f" Then
+            MessageBox.Show("You must enter f or h")
+        End If
+        If intItem = 1 Then
+
+            If txtDuration.Text = "h" Then
+
+                lstBill.Items.Add("Rug           " & "       $16.00")
+                lstBill.Items.Add("DEPOSIT         " & FormatCurrency(dblDeposit))
+                lstBill.Items.Add("TOTAL")
+                lstBill.Items.Add(FormatCurrency(dblDeposit + 16))
+
+            ElseIf txtDuration.Text = "f" Then
+
+                lstBill.Items.Add("Rug           " & "       $24.00")
+                lstBill.Items.Add("DEPOSIT         " & FormatCurrency(dblDeposit))
+                lstBill.Items.Add("TOTAL")
+                lstBill.Items.Add(FormatCurrency(dblDeposit + 24))
+
+            End If
+
+        End If
+
+        If intItem = 2 Then
+
+            If txtDuration.Text = "h" Then
+
+                lstBill.Items.Add("Lawn           " & "       $12.00")
+                lstBill.Items.Add("DEPOSIT         " & FormatCurrency(dblDeposit))
+                lstBill.Items.Add("TOTAL")
+                lstBill.Items.Add(FormatCurrency(dblDeposit + 12))
+
+            ElseIf txtDuration.Text = "f" Then
+
+                lstBill.Items.Add("Lawn           " & "       $18.00")
+                lstBill.Items.Add("DEPOSIT         " & FormatCurrency(dblDeposit))
+                lstBill.Items.Add("TOTAL")
+                lstBill.Items.Add(FormatCurrency(dblDeposit + 18))
+
+            End If
+
+        End If
+
+        If intItem = 3 Then
+
+            If txtDuration.Text = "h" Then
+
+                lstBill.Items.Add("Paint           " & "       $20.00")
+                lstBill.Items.Add("DEPOSIT         " & FormatCurrency(dblDeposit))
+                lstBill.Items.Add("TOTAL")
+                lstBill.Items.Add(FormatCurrency(dblDeposit + 20))
+
+            ElseIf txtDuration.Text = "f" Then
+
+                lstBill.Items.Add("Paint           " & "       $30.00")
+                lstBill.Items.Add("DEPOSIT         " & FormatCurrency(dblDeposit))
+                lstBill.Items.Add("TOTAL")
+                lstBill.Items.Add(FormatCurrency(dblDeposit + 30))
+
+            End If
+
+        End If
+
+
+
+    End Sub
+End Class
